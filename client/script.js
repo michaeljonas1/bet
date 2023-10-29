d7ae9a236bc3e999725b4243f310014953b9dda5
michaeljonas1 <124305414+michaeljonas1@users.noreply.github.com>
2023-10-29 07:32:01 +0300

    script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,8 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chatgpt-fqrc.onrender.com/', {
+    const response = await fetch('https://chatbetgpt.onrender.com
+/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
