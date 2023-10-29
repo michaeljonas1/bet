1c80129636b676d787799cd48de0a15340088405
michaeljonas1 <124305414+michaeljonas1@users.noreply.github.com>
2023-10-29 09:06:37 +0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,8 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chatbetgpt.onrender.com
-/', {
+    const response = await fetch('https://betogpt.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
